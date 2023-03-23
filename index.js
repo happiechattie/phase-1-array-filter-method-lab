656336a1b7c049cfd3515e8d6d5a58c34fe74669
chattie <christuaik@gmail.com>
2023-03-22 20:17:45 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+const test = ["hey", "HEY", "ay"];
+
+function findMatching(a, findThis){
+    return a.filter(function(name) {
+        const nameLC = name.toLowerCase();
+        return nameLC === findThis.toLowerCase();
+    })
+}
+
+function fuzzyMatch(a, findThis){
+    const numChar = findThis.length;
+    return a.filter(function(name) {
+        return name.slice(0, numChar) === findThis;
+    })
+}
+
+function matchName(a, findThis){
+    return a.filter(function(obj){
+        return obj.name === findThis;
+    })
+}
